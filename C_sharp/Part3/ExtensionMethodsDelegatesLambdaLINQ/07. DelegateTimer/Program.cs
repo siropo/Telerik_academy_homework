5144e199209c81413ae1e3b93cc74cc26fc1fba9
Viktor <siropo1983@abv.bg>
2013-03-12 22:34:55 +0200

    added task 7 timer

--- C_sharp/Part3/ExtensionMethodsDelegatesLambdaLINQ/07. DelegateTimer/Program.cs
+++ C_sharp/Part3/ExtensionMethodsDelegatesLambdaLINQ/07. DelegateTimer/Program.cs
@@ -6,10 +6,46 @@ using System.Threading.Tasks;
 
 namespace _07.DelegateTimer
 {
+    public delegate void TimerDelegate();
+
+    public class Timer
+    {
+        private int timeSeconds { get; set; }
+        private int duration { get; set; }
+        private TimerDelegate method { get; set; }
+
+        public Timer(int duration, int timeSeconds, TimerDelegate method)
+        {
+            this.duration = duration;
+            this.timeSeconds = timeSeconds;
+            this.method = method;
+        }
+
+        public void Run()
+        {
+            for (int i = 0; i < timeSeconds; i++)
+            {
+
+                System.Threading.Thread.Sleep(duration * 1000);
+                method();
+            }
+        }
+    }
+
     class Program
     {
+        
+
         static void Main(string[] args)
         {
+            var timer = new Timer(1, 5, test);
+            timer.Run();
+
+        }
+
+        static void test()
+        {
+            Console.WriteLine("test");
         }
     }
 }
